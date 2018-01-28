1961aced8ff07a095f281fd01e19e98081ef340d
Li Kunhao <372383419@qq.com>
2018-01-28 23:28:47 +0800

    V1.2
    详细描述用到的指令

--- for_myself/change_brightness.bat
+++ for_myself/change_brightness.bat
@@ -1,3 +1,4 @@
+rem //The two commands for setting any power settings are powercfg -SetDcValueIndex and powercfg -SetAcValueIndex, depending if you want to change setting when on batteries or on AC power.
 @echo off
 for /f "tokens=*" %%i in ('powercfg -q ^| find "Power Scheme GUID"') do set pwrSchm=%%i
 set pwrSchm=%pwrSchm:~19,36%
@@ -8,8 +9,15 @@ set dsply=%dsply:~15,36%
 for /f "tokens=*" %%i in ('powercfg -q ^| find "(Display brightness)"') do set brtnss=%%i
 set brtnss=%brtnss:~20,36%
 
-rem //输入亮度值
-set /P brightness=Enter % brightness:  %=%
+Echo.Power Scheme GUID : %pwrSchm%
+Echo.Display GUID : %dsply%
+Echo.Display brightness : %brtnss%
 
+rem //输入亮度值
+
+set /P brightness=Enter Number to change System Brightness : % brightness: %=%
 powercfg -SetDcValueIndex %pwrSchm% %dsply% %brtnss% %brightness%
+
+rem //POWERCFG -SETDCVALUEINDEX <SCHEME_GUID> <SUBGROUP_GUID> <SETTING_GUID>  <SETTING_INDEX> 
+
 powercfg -S %pwrSchm%
